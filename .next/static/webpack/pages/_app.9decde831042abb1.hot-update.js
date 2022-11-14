056685ecc1931a040ff5f05f4deb46ba55674d27
Lee Pownall <pownall@hey.com>
2022-11-14 17:31:36 +0000

    wip

---
+++ .next/static/webpack/pages/_app.9decde831042abb1.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/_app",{
+
+/***/ "./src/images/avatar.jpg":
+/*!*******************************!*\
+  !*** ./src/images/avatar.jpg ***!
+  \*******************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/avatar.720804dd.jpg\",\"height\":2160,\"width\":2089,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.720804dd.jpg&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":8});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW1hZ2VzL2F2YXRhci5qcGcuanMiLCJtYXBwaW5ncyI6IjtBQUFBLCtEQUFlLENBQUMsa01BQWtNIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8uL3NyYy9pbWFnZXMvYXZhdGFyLmpwZz8yMzlkIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IHtcInNyY1wiOlwiL19uZXh0L3N0YXRpYy9tZWRpYS9hdmF0YXIuNzIwODA0ZGQuanBnXCIsXCJoZWlnaHRcIjoyMTYwLFwid2lkdGhcIjoyMDg5LFwiYmx1ckRhdGFVUkxcIjpcIi9fbmV4dC9pbWFnZT91cmw9JTJGX25leHQlMkZzdGF0aWMlMkZtZWRpYSUyRmF2YXRhci43MjA4MDRkZC5qcGcmdz04JnE9NzBcIixcImJsdXJXaWR0aFwiOjgsXCJibHVySGVpZ2h0XCI6OH07Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/images/avatar.jpg\n"));
+
+/***/ })
+
+});
